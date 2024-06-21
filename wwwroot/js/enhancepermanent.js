5aa8adb5a47bcacf762109e388fc84060f53bfdd
Bart Calixto <bart@calixto.uy>
2024-06-21 18:32:50 -0300

    Add project files.

---
+++ wwwroot/js/enhancepermanent.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    document.querySelector('#redbutton').addEventListener('click', function () {
+        document.querySelector('#enhance').classList = 'red';
+
+        console.log('red');
+    });
+    document.querySelector('#bluebutton').addEventListener('click', function () {
+        document.querySelector('#enhance').classList = 'blue';
+        console.log('blue');
+    });
+})
